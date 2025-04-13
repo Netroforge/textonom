a6174a539d5ab1be43a14a12503622a26b7d3e1a
Yevhen Tienkaiev <hronom@gmail.com>
2025-04-13 19:14:02 +0300

    Remove Greeting class as part of codebase cleanup

--- composeApp/src/desktopMain/kotlin/com/github/netroforge/textonom/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.github.netroforge.textonom
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
